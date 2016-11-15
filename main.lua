99747f43a6f4224ba375babea03c4df3c341c585
Scott Harrrison2 <scottharrrison2@Scotts-MacBook-Pro-5.local>
2016-11-14 20:44:39 -0600

    1.3
    6.0 support

--- main.lua
+++ main.lua
@@ -1,8 +1,35 @@
 local voiceToText = require "plugin.voiceToText"
-
 local myText2 = display.newText( "blank", display.contentCenterX, display.contentCenterY-100 , native.systemFont )
-
 local myText = display.newText( "Start", display.contentCenterX, display.contentCenterY , native.systemFont )
+local showPermissionPopup
+local microphoneAccess = false
+
+local function appPermissionsListener( event )
+    
+    for k,v in pairs( event.grantedAppPermissions ) do
+        if ( v == "Microphone" ) then
+            microphoneAccess = true
+            print( "Microphone permission granted!" )
+        end
+    end
+    if microphoneAccess == false then
+        print( "Microphone permission not granted" )
+    end
+end
+
+function showPermissionPopup(  )
+    local options =
+    {
+        appPermission = "Microphone",
+        urgency = "Critical",
+        listener = appPermissionsListener,
+        rationaleTitle = "Microphone access required",
+        rationaleDescription = "Microphone access is required to take photos. Re-request now?",
+        settingsRedirectTitle = "Alert",
+        settingsRedirectDescription = "Without the ability to use microphone, this app cannot properly function. Please grant microphone access within Settings."
+    }
+    native.showPopup( "requestAppPermission", options )
+end
 
 myText:addEventListener("tap", function(e)
     if(myText.text == "Start")then
@@ -19,12 +46,17 @@ voiceToText.init(function(e)
     if(e) then
         print("init")
         print("--------")
-        print(json.prettify(e))
+        print(json.encode(e))
         print("--------")
     end
     if e.speech then
         myText2.text =e.speech
     end
+    if e.response == "stopped" then
+        myText.text ="Start"
+    end
 
 end)
 
+--show permission
+showPermissionPopup(  )
